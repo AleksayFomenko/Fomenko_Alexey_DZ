8957797ebfb9d14058d084bee5209ba5e9bed33c
Alex <fomenkolex@gmail.com>
2025-10-23 00:56:34 +0300

    Commit_2

--- HW3/DZ3.py
+++ HW3/DZ3.py
@@ -1,3 +1,11 @@
 import pandas as pd
-df = pd.read_csv('Electronic_sales_Sep2023-Sep2024.csv', sep = ',')
-print(df.head(5)) 
+df = pd.read_csv('Electronic_sales_Sep2023-Sep2024.csv', sep=',',  header=0, date_format='%Y-%m-%d')
+
+'''
+#если надо вычислять 
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+
+# Теперь можем выполнить вычисления
+df['high_low_diff'] = df['high'] - df['low']
+'''
