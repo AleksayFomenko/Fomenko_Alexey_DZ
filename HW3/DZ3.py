c4ff4bc2d8211e8b3b3ba9632721b4b72868bb53
Alex <fomenkolex@gmail.com>
2025-10-23 14:46:13 +0300

    Comit_2

--- HW3/DZ3.py
+++ HW3/DZ3.py
@@ -1,3 +1,25 @@
 import pandas as pd
-df = pd.read_csv('Electronic_sales_Sep2023-Sep2024.csv', sep = ',')
-print(df.head(5)) 
+df = pd.read_csv('Electronic_sales_Sep2023-Sep2024.csv', sep=',', header=0, date_format='%Y-%m-%d')
+
+# Определения предпочитаемого метода оплаты на основе потраченных денег
+
+df.dropna(subset=['Payment Method', 'Total Price', 'Add-on Total'], inplace=True)
+Grouped_pay_methods = df.groupby(by = [ 'Customer ID', 'Payment Method' ]).agg(
+    {
+        'Total Price' : 'sum'
+    }
+)
+Pref_payment_ind = Grouped_pay_methods.groupby("Customer ID")['Total Price'].idxmax()
+Pref_payment_method = Grouped_pay_methods.loc[Pref_payment_ind].reset_index()
+
+
+Sum_and_ads = df.groupby('Customer ID').agg(
+    {
+        'Total Price' : 'sum',
+        'Add-on Total' : 'sum'
+    }
+).reset_index()
+
+
+result = pd.merge(Pref_payment_method.drop('Total Price',axis=1), Sum_and_ads, on="Customer ID")
+print(result)
