e045154fe676c99f13f9a5f59651988330fb2825
Alex <fomenkolex@gmail.com>
2025-10-23 19:04:43 +0300

    Commit_1

--- HW4/DZ4_1.py
+++ HW4/DZ4_1.py
@@ -1,6 +1,20 @@
 import re
-car_id_1 = 'А222BС96'
-car_id_2 = 'АБ22ВВ193'
-
-matches = re.findall(r'[A-ZА-Я]+','AАGTЯTЕЕТ')
-print(matches)
+car_nambers = [ 'АБ22ВВ193',  
+    'А222BС96',  
+    'А123ВВ777', 
+    'А222ZС96',
+    'АБ22ВВ1',    
+    'АБ22ВВ1934',
+    'АБ22ВВ19',   
+    'А022ВВ7799',     
+    'АБ22ВВ193X', 
+    'А227ВВ19А222BС96',  
+]
+letter_patern = r'[АВЕКМНОРСТХУABEKMHOPCTXY]'
+full_patern = letter_patern + r'\d{3}' + letter_patern + r'{2}\d{2,3}$'
+for full_namber in car_nambers:
+    namb = re.fullmatch(full_patern, full_namber)
+    if namb:
+        print(f'Результат: Номер {namb.group()[0:6]} валиден. Регион {namb.group()[6:]}.') 
+    else:
+        print(f'Результат: Номер не валиден.')
